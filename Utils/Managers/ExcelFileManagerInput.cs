ad0a2480cc97577f2859576795287b34323dc076
Fedor Ishchenko <aleks180700@gmail.com>
2020-06-01 03:12:52 +1000

    ExcelFileManager class was added for I/O managment
    There are two patialsParts:
    1. ExcelFileManagerInput - for input
    2. ExcelFileManagerOutput - for output

---
+++ Utils/Managers/ExcelFileManagerInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.Managers
+{
+	/// <summary>
+	/// Input part
+	/// </summary>
+	public static partial class ExcelFileManager
+	{
+	}
+}
---
+++ Utils/Managers/ExcelFileManagerOutput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.Managers
+{
+	public static partial class ExcelFileManager
+	{
+	}
+}
