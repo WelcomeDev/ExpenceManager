770942a7b4e9ad1a0f1c5ccb5ea28a1700df469a
Fedor Ishchenko <aleks180700@gmail.com>
2020-06-03 14:43:32 +1000

    AddGoodPage's location was found

---
+++ ExpenceManager/AdditionalContrils/AddNewGoodTypePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenceManager.AdditionalContrils
+{
+	/// <summary>
+	/// Логика взаимодействия для AddNewGoodTypePage.xaml
+	/// </summary>
+	public partial class AddNewGoodTypePage : Page
+	{
+		public AddNewGoodTypePage()
+		{
+			InitializeComponent();
+		}
+
+		private void MainPanel_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			MainPanel.Focus();
+		}
+	}
+}
